11fa0257ae272b160ff6be92808fb2a4584d7842
Petri Zsolt <petrizsolt@outlook.com>
2024-07-03 17:25:45 +0200

    init spring app

---
+++ src/main/java/com/survey/statistics/StatisticsFromCsvApplication.java
@@ -0,0 +1,13 @@
+package com.survey.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsFromCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsFromCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/survey/statistics/StatisticsFromCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.survey.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsFromCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
